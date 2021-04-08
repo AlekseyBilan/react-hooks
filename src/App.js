512da691e341bace2a0fa51a7ee97542533faec1
oleksii bilan <bilan.aleksey.sergeevich@gmail.com>
2021-04-08 22:59:05 +0300

    add todos to the localStorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoList from './TodoList'
+import UseEffect from './UseEffect'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
-  }
+export default function App() {
+// state = { todos: [...], todoText: 'Write your todo here'}
+  let todosDefault = JSON.parse(localStorage.getItem('todos')) || [];
+  
+  // [
+  //     {id: 1, title: 'First todo', completed: false},
+  //     {id: 2, title: 'Second todo', completed: true},
+  //   ];
 
-  render() {
+//this.state.todos
+// todos
+
+// this.setState({...})
+// setTodo({...})
+
+    const [todos, setTodo] = useState(todosDefault);
+    const [todoText, setTodoText] = useState('');
+
+    const onKeyPressHandler = (e) => {
+        if ( e.which === 13 ) {
+          setTodo([...todos, {id: new Date(), title: todoText, completed: false}]);
+          setTodoText('');
+        }
+    }
+
+    const onChangeHandler = (e) => {
+      setTodoText(e.target.value);
+    }
+
+    useEffect( () => {
+          localStorage.setItem('todos', JSON.stringify(todos));
+        }           
+    )
+// [{1}, _, {3}, {4}]
+
     return (
       <div className="container">
-        <h1>Todo app</h1>
+        <h1>Todo app; TodoCount: {todos.length} </h1>
 
           <div className="input-field">
-            <input type="text" />
+            <input 
+              type="text"
+              onKeyPress={onKeyPressHandler} 
+              onChange={onChangeHandler} 
+              value={todoText}
+              placeholder="Write your todo here"
+            />
             <label>Todo name</label>
           </div>
 
-          <TodoList todos={this.state.todos} />
+          <TodoList todos={todos} />
+          <UseEffect/>
       </div>
     );
-  }
+  
 }
